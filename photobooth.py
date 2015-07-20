08ce8a01906a580d984a8d2c53dd2a28fd0795f3
pedro1713 <pedro1713@gmail.com>
2015-07-20 20:27:17 +0000

    Added shutdown function and minor fixes

--- photobooth.py
+++ photobooth.py
@@ -1,3 +1,4 @@
+import glob
 import time
 import RPi.GPIO as GPIO
 import picamera
@@ -8,55 +9,63 @@ import os
 GPIO.setmode(GPIO.BCM)
 light1_pin = 4
 light2_pin = 17
-light3_pin = 18
+#light3_pin = 18
 button1_pin = 22
 button2_pin = 23
-button3_pin = 24
+#button3_pin = 24
 GPIO.setup(light1_pin, GPIO.OUT)
 GPIO.setup(light2_pin, GPIO.OUT)
-GPIO.setup(light3_pin, GPIO.OUT)
+#GPIO.setup(light3_pin, GPIO.OUT)
 GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#GPIO.setup(button3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.output(light1_pin, False)
 GPIO.output(light2_pin, False)
-GPIO.output(light3_pin, False)
+#GPIO.output(light3_pin, False)
 
 ###Variables
 total_pics = 4
 capture_delay = 2
-prep_delay = 3
+prep_delay = 2
 real_path = os.path.dirname(os.path.realpath(__file__))
 
 ###Functions
-# define the photo taking function for when the big button is pressed 
-def start_photobooth(): 
-	################################# Begin Step 1 ################################# 
+# define the shutdown function
+def shutdown(channel):
+	print "Bye Bye Bye"
+	GPIO.output(light1_pin, True)
+	GPIO.output(light2_pin, True)
+	time.sleep(3)
+	os.system("sudo halt")
+
+# define the photo taking function for when the big button is pressed
+def start_photobooth():
+	################################# Begin Step 1 #################################
 	print "Get Ready"
-	GPIO.output(light1_pin,True);
-	time.sleep(prep_delay) 
 	GPIO.output(light1_pin,False)
-	
+	time.sleep(prep_delay)
+	#GPIO.output(light1_pin,False)
+
 	camera = picamera.PiCamera()
 	pixel_width = 640 #use a smaller size to process faster
-	pixel_height = 480 
-	camera.resolution = (pixel_width, pixel_height) 
+	pixel_height = 480
+	camera.resolution = (pixel_width, pixel_height)
 	camera.vflip = True
 	camera.hflip = False
 	#camera.saturation = -100 # comment out this line if you want color images
 	camera.start_preview()
-	
+
 	time.sleep(2) #warm up camera
 
 	################################# Begin Step 2 #################################
-	print "Taking pics" 
-	now = time.strftime("%Y-%m-%d-%H:%M:%S") #get the current date and time for the start of the filename
+	print "Taking pics"
+	now = time.strftime("%Y-%m-%d-%H%M%S") #get the current date and time for the start of the filename
 	try: #take the photos
-		for i, filename in enumerate(camera.capture_continuous('/home/pi/pics/' + now + '-' + '{counter:02d}.jpg')):
-			#GPIO.output(led2_pin,True) #turn on the LED
+		for i, filename in enumerate(camera.capture_continuous('/home/pi/pics/' + now  +  '-' + '{counter:02d}.jpg')):
+			GPIO.output(light2_pin,True) #turn on the LED
 			print(filename)
-			#time.sleep(0.25) #pause the LED on for just a bit
-			#GPIO.output(led2_pin,False) #turn off the LED
+			time.sleep(0.25) #pause the LED on for just a bit
+			GPIO.output(light2_pin,False) #turn off the LED
 			time.sleep(capture_delay) # pause in-between shots
 			if i == total_pics-1:
 				break
@@ -65,10 +74,16 @@ def start_photobooth():
 		camera.close()
 
 
-###Main Program
+### Main Program
+GPIO.add_event_detect(button2_pin, GPIO.FALLING, callback=shutdown, bouncetime=300)
+
+#delete files in pics folder on startup
+files = glob.glob('/home/pi/pics/' + '*')
+for f in files:
+	os.remove(f)
+
 while True:
+	GPIO.output(light1_pin, True)
         GPIO.wait_for_edge(button1_pin, GPIO.FALLING)
 	time.sleep(0.2) #debounce
 	start_photobooth()
-
-
